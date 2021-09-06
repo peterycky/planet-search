af22766241c0a872fb34394a8772a63b303923c4
Piotr Petrycki <peterycky@gmail.com>
2021-09-06 02:50:18 +0200

    Release 1.0

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const prepURL = name => encodeURI(`/planet/${name}`)
+
+const PlanetCard = ({item}) => {
+  return(
+    <Link className="card" to={prepURL(item.name)}>
+      <h2>{item.name}</h2>
+      <div className="card-details">
+        <p>{item.gravity}</p>
+        <p>{item.created}</p>
+      </div>
+    </Link>
+  )
+}
+
+export default PlanetCard
